e2a1689fc6d5e66da15209175836e2cbbd6320d3
MarcAttack <marcuhart@gmail.com>
2017-11-07 13:26:04 +0100

    Initiale

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        /// <summary>
+        /// GET : /HelloWorld/
+        /// Action par défaut du controller HelloWorld
+        /// L'action par défaut est toujours valorisé par la méthode Index()
+        /// </summary>
+        /// <returns></returns>
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        // NB : Pas de surcharge possible des Actions
+
+        /*
+        /// <summary>
+        /// Action Welcome simple
+        /// GET : /HelloWorld/Welcome
+        /// </summary>
+        /// <returns>HTML</returns>
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        /// <summary>
+        /// Action Welcome avec une query string.
+        /// Utilise une Query string
+        /// GET : /HelloWorld/Welcome?name=Marc&numTimes=10
+        /// </summary>
+        /// <param name="name">Nom</param>
+        /// <param name="numTimes">Numerique</param>
+        /// <returns>HTML</returns>
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTime is: " + numTimes);
+        }
+        */
+
+        /// <summary>
+        /// Action Welcome avec un paramètre id et une query string
+        /// GET : /HelloWorld/Welcome/3?name=Toto
+        /// </summary>
+        /// <param name="name">query string</param>
+        /// <param name="id">Identifiant</param>
+        /// <returns>HTML</returns>
+        public string Welcome(string name, int id = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + id);
+        }
+    }
+}
