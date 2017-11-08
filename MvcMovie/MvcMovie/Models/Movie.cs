fd049b7cafef33d20d25520d1a2aa30ae29fef3a
MarcAttack <marcuhart@gmail.com>
2017-11-08 17:14:27 +0100

    Adding Validation

--- MvcMovie/MvcMovie/Models/Movie.cs
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -7,15 +7,20 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [Required, StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
-        [Display(Name = "Date de sortie")]
-        [DataType(DataType.DateTime)]
-        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+
+        [Display(Name = "Date de sortie"), DataType(DataType.DateTime), DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
+        [Required, RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$"), StringLength(30)]
         public string Genre { get; set; }
-        [Display(Name = "Prix")]
-        [DataType(DataType.Currency)]
+
+        [Display(Name = "Prix"), Range(1, 100), DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [Required, RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$"), StringLength(5)]
         public string Rating { get; set; }
     }
 
