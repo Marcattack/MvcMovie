c56b73422c66c9a0827add62b08b456813b3ee0f
MarcAttack <marcuhart@gmail.com>
2017-11-08 15:47:38 +0100

    Adding a New Field

--- MvcMovie/MvcMovie/Models/Movie.cs
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -8,12 +8,15 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [Display(Name = "Date de sortie")]
+        [DataType(DataType.DateTime)]
         [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Display(Name = "Prix")]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 
     public class MovieDBContext : DbContext
